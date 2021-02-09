56cc9a3c47353bb520ff40b466da16eedc04972a
Aashish Bista <aashish.bista@ebpearls.com>
2021-02-09 17:49:28 +0545

    Launch Screen Added

---
+++ ConceptCodingFruitsAndBerries/Modules/Auth/Signup/SignupController.swift
@@ -0,0 +1,9 @@
+//
+//  SignupController.swift
+//  ConceptCodingFruitsAndBerries
+//
+//  Created by ebpearls on 09/02/2021.
+//  Copyright © 2021 ebpearls. All rights reserved.
+//
+
+import Foundation
--- ConceptCodingFruitsAndBerries/Modules/Base/BaseUIElementsConfig.swift
+++ ConceptCodingFruitsAndBerries/Modules/Base/BaseUIElementsConfig.swift
@@ -1,14 +1,9 @@
 //
-//  AppCoordinator.swift
+//  BaseUIElementsConfig.swift
 //  ConceptCodingFruitsAndBerries
 //
-//  Created by ebpearls on 21/01/2021.
+//  Created by ebpearls on 09/02/2021.
 //  Copyright © 2021 ebpearls. All rights reserved.
 //
 
 import Foundation
-import UIKit
-
-class AppCoordinator{
-    
-}
